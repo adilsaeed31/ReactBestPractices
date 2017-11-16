2814bc20f46485b9fa458e798fa4fca84344c754
Muhammad Adil <adilsaeed31@gmail.com>
2017-11-16 18:02:18 +0500

    Added port for heroku

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const express = require('express');
 //Create Our App
 let app = express();
 
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] == 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express is up on 3000 port');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log('Express is up on '+ PORT +' port');
 });
